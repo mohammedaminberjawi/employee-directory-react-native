7f776df1a674d07f7a0650218a293069492f5a1f
Mohammed Amin Berjawi <mohammedaminberjawi@gmail.com>
2023-05-18 12:11:33 +0300

    Add update and create apis

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,8 +1,49 @@
 import axios from 'axios';
+import {Employee} from '../store/employees-context';
 
 export const BASE_URL: string = 'http://localhost:8080';
 
 export async function fetchEmployees() {
   const response = await axios.get(`${BASE_URL}/api/employees`);
   return response.data;
+}
+
+export async function createEmployee(employee: Employee) {
+  const response = await axios.post(
+    `${BASE_URL}/api/employees`,
+    prepareFormData(employee),
+  );
+
+  return response.data;
+}
+
+export async function updateEmployee(id: string, employee: Employee) {
+  const response = await axios.put(
+    `${BASE_URL}/api/employees/${id}`,
+    prepareFormData(employee),
+  );
+
+  return response.data;
+}
+
+function prepareFormData(employee: Employee) {
+  const employeeData = new FormData();
+
+  employeeData.append('firstName', employee.firstName);
+  employeeData.append('lastName', employee.lastName);
+  employeeData.append('birthDate', employee.birthDate);
+  employeeData.append('title', employee.title);
+  employeeData.append('department', employee.department);
+  employeeData.append('location', employee.location);
+  employeeData.append('email', employee.email);
+
+  if (employee.updateImage)
+    employeeData.append('profileImage', {
+      uri: employee.updateImage.uri,
+      name: employee.updateImage.fileName,
+      filename: employee.updateImage.fileName,
+      type: employee.updateImage.type,
+    });
+
+  return employeeData;
 }
