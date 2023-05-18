3cc1e71321e6fa81623bc5611cf4ee30c5b0972d
Mohammed Amin Berjawi <mohammedaminberjawi@gmail.com>
2023-05-18 11:20:23 +0300

    Add global styles

---
+++ src/constants/styles.ts
@@ -0,0 +1,13 @@
+export const GlobalStyles = {
+  colors: {
+    royalBlue: '#4169E1',
+    mediumBlue: '#8DA5ED',
+    lightBlue: '#ECF0FC',
+    errorLight: '#EA7A96',
+    errorDark: '#87273F',
+    white: '#FFFFFF',
+    lightGray: '#A9A9A9',
+    darkGray: '#7A7A7A',
+    black: '#000000',
+  },
+};
